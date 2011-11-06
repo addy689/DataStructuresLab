9f9f03b5de5aa7f019e042da90c980ad2bac476f
Addy Singh <addy689@gmail.com>
2011-11-06 06:18:00 +0530

    Heap Libraries, Function Lib added. PriorityQ Lib declaration added

---
+++ Functions/funclib.h
@@ -0,0 +1,11 @@
+/* List of reusable functions */ 
+
+#include<stdio.h>
+
+//GENERAL
+void swap(int*,int*);	//swaps contents of two memory locations
+
+//BINARY TREE
+int left(int);		//computes left child index of a given node index
+int right(int);		//computes right child index of a given node index
+int parent(int);	//computes parent index of a given node index
---
+++ Main/heapsort.c
@@ -0,0 +1,27 @@
+#include "../HeapLib/heaplib.h"
+
+//Sample main program to input an array, construct a heap using the array, and display the values in the heap in descending order 
+
+main()
+{
+    int i,H[20];
+    
+    printf("Input array length: ");
+    scanf("%d",&len);
+	
+    printf("\nInput array -\n");
+    for(i=0;i<len;i++)
+        scanf("%d",&H[i]);
+    
+    heapsize=len;
+    build_min_heap(H);
+    printf("\nDisplaying heap -\n");
+    display_heap(H);
+    
+    printf("\nArray sorted in descending order (using Min Heap Sort) -\n");
+    minheap_sort(H);
+    display_heap(H);
+
+            
+    printf("\n");
+}

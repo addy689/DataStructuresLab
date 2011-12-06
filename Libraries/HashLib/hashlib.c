8fe205e95870ba378ee635de1747a38a3c44c456
Addy Singh <addy689@gmail.com>
2011-12-06 12:26:51 +0530

    Completed Hash Tables Implementation (with deletion)

--- Libraries/HashLib/hashlib.c
+++ Libraries/HashLib/hashlib.c
@@ -6,19 +6,32 @@
 
 void insertData(struct Node **H, char *str)
 {
-	int k;
+	int k,flag;
 	
 	k = hashData(str[0]);
-	printf("%d",k);
-	printf("\n%s",str);
-
-	struct Node *N;
-	allocateNode(&N,strlen(str));
+	flag = 0;
 	
-	strcpy(N->data,str);		//input data is copied into the new node
-	N->next = H[k];			//first node in the existing chain is assigned as the next node to the new node
+	struct Node *ptr;
+	ptr = searchChain(H,k,str,&flag);
 	
-	H[k] = N;					//new node is inserted at the Head of the chain
+	if(ptr!=NIL)
+	{
+		if(flag!=1)
+			ptr = ptr->next;
+			
+		(ptr->ct)++;
+	}
+	
+	else
+	{
+		struct Node *N;
+		allocateNode(&N,strlen(str));
+	
+		strcpy(N->data,str);		//input data is copied into the new node
+		N->next = H[k];			//first node in the existing chain is assigned as the next node to the new node
+	
+		H[k] = N;					//new node is inserted at the Head of the chain
+	}
 }
 
 int hashData(char ch)
@@ -30,8 +43,54 @@ int hashData(char ch)
 void allocateNode(struct Node ** M,int len)
 {
 	(*M) = malloc(sizeof(struct Node));
+	(*M)->ct = 1;
 	(*M)->data = malloc(sizeof(char)*len);
 	(*M)->next = NIL;
+}
+
+void deleteData(struct Node ** H,char *del)
+{
+	int k,flag;
+	
+	k = hashData(del[0]);
+	flag = 0;
+	
+	struct Node *ptr,*nptr;
+	ptr = searchChain(H,k,del,&flag);
+	
+	if(ptr!=NIL)
+	{
+			if(flag==1)
+			{
+					if((ptr->ct)==1)
+					{
+						H[k] = ptr->next;
+						free(ptr);
+					}
+			
+					else
+						(ptr->ct)--;
+			}
+		
+			else
+			{
+					nptr = ptr->next;
+			
+					if(nptr->ct==1)
+					{
+						ptr->next = nptr->next;
+						free(nptr);
+					}
+			
+					else
+						(nptr->ct)--;
+			}
+			
+			printf("\nAn item has been deleted!");
+	}
+	
+	else
+		printf("\nThe item was not found!!");
 }
 
 void displayTable(struct Node ** H)
@@ -47,10 +106,75 @@ void displayTable(struct Node ** H)
 		ptr = H[i];
 		while(ptr!=NIL)
 		{
-			printf("\t%s",ptr->data);
+			printf("\t%s(%d)",ptr->data,ptr->ct);
 			ptr = ptr->next;
 		}
 	}
 }
 
-/*void deleteData(struct Node *)*/
+struct Node* searchChain(struct Node ** H,int k,char *str,int *flag)
+{
+	struct Node *loc,*nextloc;
+	loc = H[k];
+	
+	if(loc!=NIL)
+	{
+		if(!strcmp(loc->data,str))
+		{
+			(*flag) = 1;
+			return loc;
+		}
+	}
+	
+	while(loc!=NIL)
+	{
+		nextloc = loc->next;
+		if(nextloc!=NIL)
+			if(!strcmp(nextloc->data,str))
+				return loc;
+		
+		loc = loc->next;
+	}
+	
+	return loc;
+}
+
+void parse(struct Node ** H,char *str)
+{
+	int i,j,flg;
+	char S[30];
+	
+	i = 0;
+	j = 0;
+	flg = 0;
+	while(str[i]!='\0')
+	{
+		while(isalnum(str[i]))
+		{
+			S[j] = str[i];
+			j++;
+			i++;
+			
+			if(str[i]=='\0')
+			{
+				flg = 1;
+				break;
+			}
+		}
+		
+		if(j!=0)
+		{
+			S[j] = '\0';
+			
+			if(isalpha(S[0]))
+				insertData(H,S);
+			
+			j = 0;
+		}
+		
+		if(flg==1)
+			break;
+		
+		i++;
+	}
+}
--- Libraries/HashLib/hashlib.h
+++ Libraries/HashLib/hashlib.h
@@ -5,20 +5,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <ctype.h>
 
 #define m 26
 #define size 255
 
 //Structure that defines a node in a Hash Table chain
 struct Node{
+		int ct;
 		char *data;							//the data to be stored in the Hash Table
 		struct Node *next;					//pointer to the next node in the chain
 	};
 
 struct Node *NIL;							//to be used as NULL pointer
 
-void insertData(struct Node **,char *);					//inserts the input data
-int hashData(char);						//applies the hashing function on the input data (on data[0] actually)
+void insertData(struct Node **,char *);			//inserts the input data
+int hashData(char);								//applies the hashing function on the input data (on data[0] actually)
 void allocateNode(struct Node **,int);			//allocates memory to a node at runtime
-/*void deleteData(struct Node *)*/
-void displayTable(struct Node **);			//displays the chains corresponding to each hash table slot
+void deleteData(struct Node **,char *);
+void displayTable(struct Node **);				//displays the chains corresponding to each hash table slot
+
+struct Node* searchChain(struct Node **,int,char *,int *);			//searches a slot's chain for the given data
+void parse(struct Node **,char *);									//parses the data according to certain rules
